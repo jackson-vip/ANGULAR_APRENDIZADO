9e88214c2cbe314f92c259810fd8dacc47cae669
jackson_vip <jackson.tiburtinopb@gmail.com>
2025-09-23 14:48:05 -0300

    feat: implementa exemplos de condicionais com *ngIf e nova sintaxe @if
    - Adiciona exemplos da diretiva *ngIf (sintaxe tradicional)
    - Implementa nova sintaxe @if do Angular 17+
    - Demonstra uso de else com ng-template e @else
    - Compara diferenças entre sintaxes antigas e atuais
    - Configura CommonModule para suporte às diretivas condicionais

--- ngIf/src/app/app-module.ts
+++ ngIf/src/app/app-module.ts
@@ -1,5 +1,6 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
@@ -10,7 +11,8 @@ import { App } from './app';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    // CommonModule,
+    AppRoutingModule,
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
--- ngIf/src/app/app.ts
+++ ngIf/src/app/app.ts
@@ -8,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class App {
   protected title = 'ngIf';
+
+  num: number = undefined as unknown as number;
+  minhaProp: boolean = true;
+
+  toggleProp() {
+    this.minhaProp = !this.minhaProp;
+  }
+
+  /* Valores verdadeiros */
+  isTrufy() {
+    return true;
+    // return {};
+    // return [];
+    // return 1;
+    // return "false";
+    // return "0";
+    // return ' ';
+  }
+
+  isFalsy() {
+    return false;
+    // return null;
+    // return undefined;
+    // return 0;
+    // return '';
+    // return -0;
+    // return NaN;
+  }
+
 }
