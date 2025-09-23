ccca491eb2e8250d24d43d7b8c2edb251f98e77e
jackson_vip <jackson.tiburtinopb@gmail.com>
2025-09-23 08:54:43 -0300

    feat: adiciona suporte a two-way data binding no componente App

--- two-way-data-binding/src/app/app.ts
+++ two-way-data-binding/src/app/app.ts
@@ -8,4 +8,21 @@ import { Component, signal } from '@angular/core';
 })
 export class App {
   protected readonly title = signal('two-way-data-binding');
+
+  nome: string = 'Jackson2';
+
+  /**
+   * Handle input change event
+   * @param event - The input event value
+   * @returns void
+   * O Método que vai lidar com o evento de mudança no input
+   * e vai atualizar o valor da propriedade nome
+   * convertendo o valor para maiúsculas
+   * 
+   */
+  handleInputChange( event: string ) {
+    console.log(event);
+    const nameUpcased = event.toUpperCase();
+    this.nome = nameUpcased;
+  }
 }
