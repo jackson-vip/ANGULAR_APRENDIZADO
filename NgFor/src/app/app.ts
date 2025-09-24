fdfd4a2933e9444922ad6e6c49b43eeed9d68e37
jackson_vip <jackson.tiburtinopb@gmail.com>
2025-09-24 08:04:02 -0300

    feat: Aprendizado do NgFor
    -  sintaxe do NgFor que será descontinuada, porém usada em projetos legados
    -  sintaxe atual para versão >= ao Angular 17+

--- NgFor/src/app/app.ts
+++ NgFor/src/app/app.ts
@@ -8,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class App {
   protected title = 'NgFor';
+
+  personSelectIndex: number | undefined;
+
+  listPeople: Array<{ nome: string; idade: number }> = [
+    { nome: 'João', idade: 25 },
+    { nome: 'Maria', idade: 30 },
+    { nome: 'Pedro', idade: 22 },
+    { nome: 'Ana', idade: 28 },
+  ];
+  
+  selectPerson( index: number ) {
+    console.log(index);
+    this.personSelectIndex = index;
+  }
 }
