b5e83ea74780a59c34e63d078402a5e14374e17a
Jackson Ferreira <jackson.tiburtinopb@gmail.com>
2025-10-28 17:40:24 -0300

    feat: Atualiza configuração do DatePipe e adiciona suporte a locale pt-BR
    - Date Pipe Parte 3: Location e Injection Tokens

--- date-pipe/src/app/app.module.ts
+++ date-pipe/src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+
+import { DATE_PIPE_DEFAULT_OPTIONS, DatePipeConfig } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+const datePipeConfig: DatePipeConfig = {
+  dateFormat: "dd/MM/yyyy",
+  timezone: '+0000',
+};
+
+registerLocaleData(localePt, 'pt-BR');
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR',
+    },
+    {
+      provide: DATE_PIPE_DEFAULT_OPTIONS,
+      useValue: datePipeConfig
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
