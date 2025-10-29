e82cf90f5f198fa9c0f454f6ccb09797a7cd1699
Jackson Ferreira <jackson.tiburtinopb@gmail.com>
2025-10-29 16:11:58 -0300

    feat: atualizar módulo e componente para implementar CurrencyPipe com localização
    - Adicionada configuração de locale para português (pt-BR) em app.module.ts.
    - Registrado locale data para pt-BR, garantindo formatação correta de datas, números e moedas.
    - Definido BRL como moeda padrão para o CurrencyPipe.
    - Atualizado app.component.html para demonstrar diferentes formatos de moeda: CAD, BRL e USD.

--- currency-pipe/src/app/app.module.ts
+++ currency-pipe/src/app/app.module.ts
@@ -1,7 +1,27 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID, DEFAULT_CURRENCY_CODE } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData, CurrencyPipe } from '@angular/common';
+
 import { AppComponent } from './app.component';
+
+// Configuração do locale para português (pt-BR)
+// (necessário para formatação correta de datas, números e moedas)
+const LOCALE_PROVIDER_PT_BR = {
+  provide: LOCALE_ID,
+  useValue: 'pt-BR',
+};
+// Registrar os dados do locale pt-BR no Angular 
+// (necessário para formatação correta de datas, números e moedas)
+registerLocaleData(localePt, 'pt-BR');
+
+// Configuração do CurrencyPipe para utilizar o locale pt-BR por padrão
+// (necessário para exibir valores monetários no formato brasileiro)
+const CURRENCY_PIPE_PROVIDER_PT_BR = {
+  provide: DEFAULT_CURRENCY_CODE,
+  useValue: 'BRL',
+};
 
 @NgModule({
   declarations: [
@@ -10,7 +30,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [LOCALE_PROVIDER_PT_BR, CURRENCY_PIPE_PROVIDER_PT_BR, CurrencyPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
