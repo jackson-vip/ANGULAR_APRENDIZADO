e24e7e17d55f4710f5abee725b143eeead68a234
jackson_vip <jackson.tiburtinopb@gmail.com>
2025-10-30 09:22:02 -0300

    feat: adicionar suporte ao Async Pipe e refatorar componente para uso de Observable

--- async-pipe/src/app/components/obs-com-objeto/obs-com-objeto.component.ts
+++ async-pipe/src/app/components/obs-com-objeto/obs-com-objeto.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { UsersService } from '../../services/users.service';
 import { IUserResponse } from '../../interfaces/user/user-response.interface';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-obs-com-objeto',
@@ -10,14 +10,31 @@ import { Subscription } from 'rxjs';
 })
 export class ObsComObjetoComponent implements OnInit, OnDestroy {
 
+  /** Boa pratica: 
+   * 1. Criar as PROPRIEDADES do componente
+   * 2. Criar o CONSTRUTOR
+   * 3. Criar os MÉTODOS do ciclo de vida (ngOnInit, ngOnDestroy, etc)
+   * 4. Criar os MÉTODOS customizados do componente
+   */
+
+
   user: IUserResponse = {} as IUserResponse;
   userSubscription: Subscription | undefined;
+  
+  /** Uma boa pratica ao criar um variável do tipo Observable é utilizar o sinal $ ao final do nome
+   * Exemplo: user$
+   * Isso ajuda a identificar rapidamente que a variável é um Observable
+   */
+  user$!: Observable<IUserResponse>;
 
   constructor(
     private readonly _usersService: UsersService
   ) {}
 
   ngOnInit() {
+    // Usando Async Pipe
+    this.user$ = this._usersService.getUserById(2);
+
     this._usersService.getUserById(1).subscribe(userResponse => {
       // console.log(userResponse);
       this.user = userResponse;
@@ -29,4 +46,9 @@ export class ObsComObjetoComponent implements OnInit, OnDestroy {
     // Se houver alguma assinatura, cancele-a
     this.userSubscription && this.userSubscription?.unsubscribe();
   }
+
+  onBtnClick( userId: number) {
+    this.user$ = this._usersService.getUserById(userId);
+  }
+
 }
