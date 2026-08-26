285f9c9ba640e2e6a6d404001ede2a37c5e8c8f1
Jackson Ferreira <jackson.tiburtinopb@gmail.com>
2026-08-26 16:55:38 -0300

    feat: avançando no conteúdo :
    - 278. Como Criar FormGroup Filhos
    - 279. Como Fazer o Submit do Formulário
    -  280. Como Atualizar Todos os Valores do Formulário com o Método setValue
    - 281. Como Atualizar Parcialmente os Valores do Formulário com o Método patchValue
    -  282. Como Observar Mudanças no Formulário com o Método valueChanges

--- reactive-forms/src/app/EXEMPLOS/form-group/form-group.ts
+++ reactive-forms/src/app/EXEMPLOS/form-group/form-group.ts
@@ -8,17 +8,26 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrl: './form-group.scss',
 })
 export class FormGroupComponent {
-  pesquisa = new FormControl('');
-
-
   // Criando um FormGroup com dois FormControls: nome e idade
   pessoaForm = new FormGroup({
     nome: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.email]),
+    endereco: new FormGroup({
+      rua: new FormControl('', [Validators.required]),
+      numero: new FormControl('', [Validators.required])
+    })
+  }, {
+    updateOn: 'change'
   });
 
   constructor() {
     console.log('Constructor object:', this.pessoaForm.get('nome'))
+    
+    // Quando realizamos alterações no formulário, o valueChanges emite o novo valor.
+    this.pessoaForm.valueChanges.subscribe(value => {
+      console.log('Formulário mudou:', value);
+    });
+    
   }
 
   /** Getter para acessar o FormControl do FormGroup. Essa prática ajuda a acessar 
@@ -30,11 +39,45 @@ export class FormGroupComponent {
   get email(): FormControl {
     return this.pessoaForm.get('email') as FormControl;
   }
+  get rua(): FormControl {
+    return (this.pessoaForm.get('endereco')?.get('rua') as FormControl);
+  }
+  get numero(): FormControl {
+    return (this.pessoaForm.get('endereco')?.get('numero') as FormControl);
+  }
+  
+  mostrarValor() {
+    console.log('Valor atual do formulário:', this.pessoaForm.value);
+  }
 
   onSubmit() {
     if (this.pessoaForm.valid){
       console.log('Formulário enviado:', this.pessoaForm);
       console.log('Formulário válido:', this.pessoaForm.value);      
     }
+  }
+
+  AlterarConteudo() {
+    console.log('Método AlterarConteudo chamado');
+    // O método setValue substitui todos os valores do formulário pelos fornecidos.
+    this.pessoaForm.setValue({
+      nome: 'Novo Nome',
+      email: 'novoemail@example.com',
+      endereco: {
+        rua: 'Nova Rua',
+        numero: '123'
+      }
+    });
+  }
+
+  AlterarParcial() {
+    console.log('Método AlterarParcial chamado');
+    // O método patchValue permite alterar apenas alguns valores do formulário, sem substituir todos.
+    this.pessoaForm.patchValue({
+      nome: 'Nome Parcialmente Alterado',
+      endereco: {
+        rua: 'Rua Parcialmente Alterada'
+      }
+    });
   }
 }
